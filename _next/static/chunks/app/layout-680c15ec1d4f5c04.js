2d7a10825c6075599e865b92657edfa2b59d9f09
cisdeli <pedrohcisdeli@gmail.com>
2024-02-09 00:31:10 -0600

    Deploy

---
+++ _next/static/chunks/app/layout-680c15ec1d4f5c04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7976:function(n,e,_){Promise.resolve().then(_.t.bind(_,6087,23))},6087:function(n){n.exports={style:{fontFamily:"'__Inter_a78554', '__Inter_Fallback_a78554'",fontStyle:"normal"},className:"__className_a78554"}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=7976)}),_N_E=n.O()}]);
